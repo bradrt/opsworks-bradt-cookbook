b239a2eeb179e231188ff80ff7e693cbe587a9a4
Brad Thompson <brad@youdazzle.com>
2014-02-27 18:44:06 -0800

    missing ":"

--- sample/recipes/bash.rb
+++ sample/recipes/bash.rb
@@ -1,6 +1,6 @@
 bash "change system greeting" do
   user "root"
   code <<-EOH
-  echo "#{node[:motd1]} #{node[motd2]}" >> /etc/motd
+  echo "#{node[:motd1]} #{node[:motd2]}" >> /etc/motd
   EOH
 end
