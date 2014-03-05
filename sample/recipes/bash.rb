9564c8f61bdcb9742a209d1f47c74d9a292d56c2
Brad Thompson <brad@youdazzle.com>
2014-03-05 08:10:02 -0800

    use hostname opsworks instance attribute

--- sample/recipes/bash.rb
+++ sample/recipes/bash.rb
@@ -2,5 +2,6 @@ bash "change system greeting" do
   user "root"
   code <<-EOH
   echo "#{node[:motd1]} #{node[:motd2]}" >> /etc/motd
+  echo "hostname: #{node[:opsworks][:instance][:hostname]}" >> /etc/motd
   EOH
 end
