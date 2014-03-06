60181cc605379d1a43a71fcdaf862cd28a0f9e5b
Brad Thompson <brad@youdazzle.com>
2014-03-06 09:22:15 -0800

    add version to package install

--- alertlogic/recipes/default.rb
+++ alertlogic/recipes/default.rb
@@ -1,5 +1,6 @@
 package "alertlogic" do
   action :install
+  version "latest"
   source "#{Chef::Config[:file_cache_path]}/site-cookbooks/alertlogic/files/default/al-threat-host_LATEST.amd64.deb"
 end
 
